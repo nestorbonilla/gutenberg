ae09dfcd8e3407d45734681682938403c596c862
Carl Lippert <carl.lippert@gmail.com>
2022-01-25 15:42:38 -0500

    Images.

--- src/components/book.tsx
+++ src/components/book.tsx
@@ -1,23 +1,32 @@
-const Book = ({ book }: any) => {
-  
+import { book } from "../types/mockMetadata";
+
+type Props = {
+  book: book; 
+}
+
+const Book = ({ book }: Props) => {
+  // let meta = book.metadata;
+
+  // console.log(meta);
+
   return (
-    <div key={book.id} className="group relative">
+    <div key={book.ipfs_url} className="group relative">
       <div className="w-full min-h-80 bg-gray-200 aspect-w-1 aspect-h-1 rounded-md overflow-hidden group-hover:opacity-75 lg:h-80 lg:aspect-none">
         <img
-          src={book.imageSrc}
-          alt={book.imageAlt}
+          src={book.image}
+          // alt={book.description}
           className="w-full h-full object-center object-cover lg:w-full lg:h-full"
         />
       </div>
       <div className="mt-4 flex justify-between">
         <div>
           <h3 className="text-sm text-gray-700">
-            <a href={book.href}>
+            <a href={book.image}>
               <span aria-hidden="true" className="absolute inset-0" />
               {book.name}
             </a>
           </h3>
-          <p className="mt-1 text-sm text-gray-500">{book.color}</p>
+          <p className="mt-1 text-sm text-gray-500">{book.authors}</p>
         </div>
         <p className="text-sm font-medium text-gray-900">{book.price}</p>
       </div>
