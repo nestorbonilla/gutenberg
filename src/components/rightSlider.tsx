1a8b02e7c8f5e481dbd1eeb6ca1951a0a15bdad2
Grace Wang <gracewang92@gmail.com>
2022-01-25 16:51:36 -0500

    better styling for highlights

--- src/components/rightSlider.tsx
+++ src/components/rightSlider.tsx
@@ -1,36 +1,31 @@
 /* This example requires Tailwind CSS v2.0+ */
-import { Fragment, useState } from 'react'
-import { Dialog, Menu, Transition } from '@headlessui/react'
-import { XIcon } from '@heroicons/react/outline'
-import { DotsVerticalIcon } from '@heroicons/react/solid'
+import { Dialog, Transition } from '@headlessui/react'
+import { TrashIcon, XIcon } from '@heroicons/react/outline'
+import { Fragment } from 'react'
 
-const tabs = [
-  { name: 'All', href: '#', current: true },
-  { name: 'Online', href: '#', current: false },
-  { name: 'Offline', href: '#', current: false },
-]
-const team = [
-  {
-    name: 'Leslie Alexander',
-    handle: 'lesliealexander',
-    href: '#',
-    imageUrl:
-      'https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
-    status: 'online',
-  },
-  // More people...
-]
+export interface Highlight {
+  id: number;
+  text: string;
+  cfiRange: string;
+  notes?: string;
+}
 
-function classNames(...classes) {
+function classNames(...classes: any[]) {
   return classes.filter(Boolean).join(' ')
 }
 
-export default function Example() {
-  const [open, setOpen] = useState(true)
+interface RightSliderProps {
+  highlights: Highlight[];
+  show: boolean;
+  setShow: (show: boolean) => void;
+  select: (cfiRange: string) => void;
+  delete: (cfiRange: string, id: number) => void;
+}
 
+export default function RightSlider(props: RightSliderProps) {
   return (
-    <Transition.Root show={open} as={Fragment}>
-      <Dialog as="div" className="fixed inset-0 overflow-hidden" onClose={setOpen}>
+    <Transition.Root show={props.show} as={Fragment}>
+      <Dialog as="div" className="fixed inset-0 overflow-hidden" onClose={() => props.setShow(false)} style={{ zIndex: 100 }} >
         <div className="absolute inset-0 overflow-hidden">
           <Dialog.Overlay className="absolute inset-0" />
 
@@ -48,12 +43,12 @@ export default function Example() {
                 <div className="h-full flex flex-col bg-white shadow-xl overflow-y-scroll">
                   <div className="p-6">
                     <div className="flex items-start justify-between">
-                      <Dialog.Title className="text-lg font-medium text-gray-900">Team</Dialog.Title>
+                      <Dialog.Title className="text-lg font-medium text-gray-900">Highlights</Dialog.Title>
                       <div className="ml-3 h-7 flex items-center">
                         <button
                           type="button"
                           className="bg-white rounded-md text-gray-400 hover:text-gray-500 focus:ring-2 focus:ring-indigo-500"
-                          onClick={() => setOpen(false)}
+                          onClick={() => props.setShow(false)}
                         >
                           <span className="sr-only">Close panel</span>
                           <XIcon className="h-6 w-6" aria-hidden="true" />
@@ -61,100 +56,28 @@ export default function Example() {
                       </div>
                     </div>
                   </div>
-                  <div className="border-b border-gray-200">
-                    <div className="px-6">
-                      <nav className="-mb-px flex space-x-6" x-descriptions="Tab component">
-                        {tabs.map((tab) => (
-                          <a
-                            key={tab.name}
-                            href={tab.href}
-                            className={classNames(
-                              tab.current
-                                ? 'border-indigo-500 text-indigo-600'
-                                : 'border-transparent text-gray-500 hover:text-gray-700 hover:border-gray-300',
-                              'whitespace-nowrap pb-4 px-1 border-b-2 font-medium text-sm'
-                            )}
-                          >
-                            {tab.name}
-                          </a>
-                        ))}
-                      </nav>
-                    </div>
-                  </div>
+                  <div className="border-b border-gray-200" />
                   <ul role="list" className="flex-1 divide-y divide-gray-200 overflow-y-auto">
-                    {team.map((person) => (
-                      <li key={person.handle}>
-                        <div className="relative group py-6 px-5 flex items-center">
-                          <a href={person.href} className="-m-1 flex-1 block p-1">
-                            <div className="absolute inset-0 group-hover:bg-gray-50" aria-hidden="true" />
+                    {props.highlights.map((highlight) => (
+                      <li key={highlight.cfiRange}>
+                        <div className="flex items-center">
+                          <button className="relative group py-6 px-5 flex-1 truncate" onClick={() => props.select(highlight.cfiRange)}>
+                            <div className="absolute inset-0 group-hover:bg-gray-100" aria-hidden="true" />
                             <div className="flex-1 flex items-center min-w-0 relative">
-                              <span className="flex-shrink-0 inline-block relative">
-                                <img className="h-10 w-10 rounded-full" src={person.imageUrl} alt="" />
-                                <span
-                                  className={classNames(
-                                    person.status === 'online' ? 'bg-green-400' : 'bg-gray-300',
-                                    'absolute top-0 right-0 block h-2.5 w-2.5 rounded-full ring-2 ring-white'
-                                  )}
-                                  aria-hidden="true"
-                                />
-                              </span>
                               <div className="ml-4 truncate">
-                                <p className="text-sm font-medium text-gray-900 truncate">{person.name}</p>
-                                <p className="text-sm text-gray-500 truncate">{'@' + person.handle}</p>
+                                <p className="text-sm font-medium text-gray-900 truncate">{highlight.text}</p>
                               </div>
                             </div>
-                          </a>
-                          <Menu as="div" className="ml-2 flex-shrink-0 relative inline-block text-left">
-                            <Menu.Button className="group relative w-8 h-8 bg-white rounded-full inline-flex items-center justify-center focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
-                              <span className="sr-only">Open options menu</span>
-                              <span className="flex items-center justify-center h-full w-full rounded-full">
-                                <DotsVerticalIcon
-                                  className="w-5 h-5 text-gray-400 group-hover:text-gray-500"
-                                  aria-hidden="true"
-                                />
-                              </span>
-                            </Menu.Button>
-                            <Transition
-                              as={Fragment}
-                              enter="transition ease-out duration-100"
-                              enterFrom="transform opacity-0 scale-95"
-                              enterTo="transform opacity-100 scale-100"
-                              leave="transition ease-in duration-75"
-                              leaveFrom="transform opacity-100 scale-100"
-                              leaveTo="transform opacity-0 scale-95"
-                            >
-                              <Menu.Items className="origin-top-right absolute z-10 top-0 right-9 w-48 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5 focus:outline-none">
-                                <div className="py-1">
-                                  <Menu.Item>
-                                    {({ active }) => (
-                                      <a
-                                        href="#"
-                                        className={classNames(
-                                          active ? 'bg-gray-100 text-gray-900' : 'text-gray-700',
-                                          'block px-4 py-2 text-sm'
-                                        )}
-                                      >
-                                        View profile
-                                      </a>
-                                    )}
-                                  </Menu.Item>
-                                  <Menu.Item>
-                                    {({ active }) => (
-                                      <a
-                                        href="#"
-                                        className={classNames(
-                                          active ? 'bg-gray-100 text-gray-900' : 'text-gray-700',
-                                          'block px-4 py-2 text-sm'
-                                        )}
-                                      >
-                                        Send message
-                                      </a>
-                                    )}
-                                  </Menu.Item>
-                                </div>
-                              </Menu.Items>
-                            </Transition>
-                          </Menu>
+                          </button>
+                          <button
+                            className="relative group px-5 flex self-stretch items-center justify-center"
+                            onClick={() => props.delete(highlight.cfiRange, highlight.id)}>
+                            <div className="absolute inset-0 group-hover:bg-gray-100" aria-hidden="true" />
+                            <TrashIcon
+                              className="w-5 text-gray-400 group-hover:text-gray-500 justify-content-stretch z-50"
+                              aria-hidden="true"
+                            />
+                          </button>
                         </div>
                       </li>
                     ))}
--- src/pages/reader.tsx
+++ src/pages/reader.tsx
@@ -1,12 +1,15 @@
+import { MenuIcon } from "@heroicons/react/outline";
 import { Rendition } from "epubjs";
 import type { NextPage } from "next";
 import { useEffect, useRef, useState } from "react";
 import { ReactReader } from "react-reader";
+import RightSlider, { Highlight } from "../components/rightSlider";
 // import styles from '../styles/Home.module.css'
 
 const Reader: NextPage = () => {
+  const [showHighlights, setShowHighlights] = useState(false);
   const [location, setLocation] = useState<string>();
-  const [selections, setSelections] = useState<any>([]);
+  const [selections, setSelections] = useState<Highlight[]>([]);
   const renditionRef = useRef<Rendition>();
 
   useEffect(() => {
@@ -20,6 +23,7 @@ const Reader: NextPage = () => {
 
   function setRenderSelection(cfiRange: string, contents: any) {
     setSelections(selections.concat({
+      id: selections.length,
       text: renditionRef.current!.getRange(cfiRange).toString(),
       cfiRange
     }))
@@ -42,14 +46,44 @@ const Reader: NextPage = () => {
     })
     setSelections([]);
   }
+
+  function deleteHighlight(cfiRange: string, i: number) {
+    renditionRef.current!.annotations.remove(cfiRange, 'highlight')
+    setSelections(selections.filter((item: any, j: number) => j !== i))
+  }
+
   return (
-    <div className="m" style={{ height: "100vh" }}>
-      <ReactReader
-        location={location}
-        locationChanged={locationChanged}
-        getRendition={getRendition}
-        url="https://gerhardsletten.github.io/react-reader/files/alice.epub"
+    <div>
+      <div className="m" style={{ height: "100vh" }}>
+        <ReactReader
+          location={location}
+          locationChanged={locationChanged}
+          getRendition={getRendition}
+          url="https://gerhardsletten.github.io/react-reader/files/alice.epub"
+        />
+      </div>
+      {!showHighlights && <button
+        type="button"
+        className="bg-white rounded-md text-gray-400 hover:text-gray-500 focus:ring-2 focus:ring-indigo-500"
+        onClick={() => setShowHighlights(true)}
+      >
+        <MenuIcon className="h-6 w-6" aria-hidden="true" style={{ position: 'absolute', top: '1rem', right: '1rem', zIndex: 100 }} />
+      </button>}
+      <RightSlider
+        highlights={selections}
+        show={showHighlights}
+        setShow={setShowHighlights}
+        select={(cfiRange: string) => renditionRef.current!.display(cfiRange)}
+        delete={deleteHighlight}
       />
+      <div style={{ position: 'absolute', bottom: '1rem', right: '1rem', zIndex: 1, backgroundColor: 'white' }}>
+        <ul>
+          {selections.map(({ text, cfiRange }, i: number) => (
+            <li key={i}>
+            </li>
+          ))}
+        </ul>
+      </div>
     </div>
   );
 };
