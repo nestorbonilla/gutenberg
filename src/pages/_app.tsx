db2beaa7f13ea34069e05e88f357b13c97e01446
Grace Wang <gracewang92@gmail.com>
2022-01-27 14:35:40 -0500

    also call Moralis.start lol

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,14 @@
-import { MoralisProvider } from "react-moralis";
+import Moralis from "moralis";
 import type { AppProps } from "next/app";
+import { MoralisProvider } from "react-moralis";
 import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return (  
+  Moralis.start({
+    serverUrl: "https://7fqgvttpqukt.usemoralis.com:2053/server",
+    appId: "585mUNiZ538xo3FEY7lbXWFZjjFPNxKOvUstjfhc",
+  });
+  return (
     <MoralisProvider
       appId={"585mUNiZ538xo3FEY7lbXWFZjjFPNxKOvUstjfhc"}
       serverUrl={"https://7fqgvttpqukt.usemoralis.com:2053/server"}
