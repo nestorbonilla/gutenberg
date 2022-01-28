b58d404256026fe5366dd7c0cb19b073114e88f9
Nestor Bonilla <nbonilla@Nestors-MacBook-Pro.local>
2022-01-27 21:34:07 -0500

    updating addresses and deploy script

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -13,36 +13,30 @@ async function main() {
   console.log("Library contract was deployed to: ", library.address);
 
   const GenesisCollection = await ethers.getContractFactory("GenesisCollection");
-  const genesisCollection = await GenesisCollection.connect(author).deploy("Genesis", "BOOK", "https://gateway.pinata.cloud/ipfs/", "QmfLvZMG4myTDdMQ6LTFobuY5HUyZRGYn2s4mC5ouYLZuo", library.address); // name, symbol, baseUri, ipfsFolderHash, libraryAddress
+  const genesisCollection = await GenesisCollection.connect(author).deploy("Genesis", "BOOK", "https://gateway.pinata.cloud/ipfs/", "QmQi9KooqR8wEwPsxuGdacG7wTFVDF5oBNobKSTzcrDF9Q", library.address); // name, symbol, baseUri, ipfsFolderHash, libraryAddress
   await genesisCollection.deployed();
 
   console.log("Genesis Collection contract was deployed to: ", genesisCollection.address);
 
-  await genesisCollection.connect(author).mint(10);
+  await genesisCollection.connect(author).mint(1000);
+  await genesisCollection.connect(author).mint(200);
+  await genesisCollection.connect(author).mint(500);
+  await genesisCollection.connect(author).mint(700);
 
   console.log("Genesis Collection successfully minted");
 
-  await library.connect(author).createSemiFungibleBook(genesisCollection.address, 1, 0, 10); // nftContract, tokenId, price, amount
+  await library.connect(author).createSemiFungibleBook(genesisCollection.address, 1, 0, 1000); // nftContract, tokenId, price, amount
+  await library.connect(author).createSemiFungibleBook(genesisCollection.address, 2, 0, 200);
+  await library.connect(author).createSemiFungibleBook(genesisCollection.address, 3, 0, 500);
+  await library.connect(author).createSemiFungibleBook(genesisCollection.address, 4, 0, 700);
 
   console.log("Book created at Library");
 
+  const SecondaryCollection = await ethers.getContractFactory("SecondaryCollection");
+  const secondaryCollection = await SecondaryCollection.deploy("Secondary", "BOOK", library.address); // name, symbol, libraryAddress
+  await secondaryCollection.deployed();
 
-
-
-  // const SecondaryCollection = await ethers.getContractFactory("SecondaryCollection");
-  // const secondaryCollection = await SecondaryCollection.deploy("Secondary", "BOOK", library.address); // name, symbol, libraryAddress
-  // await secondaryCollection.deployed();
-
-  // const new721BookId = await secondaryCollection.mint(""); //tokenUri
-
-  // console.log("Book with standard 721 minted");
-
-  // await library.createSemiFungibleBook(genesisCollection.address, 1, 0, 10); // nftContract, tokenId, price, units
-
-  // console.log("Book 721 created at Library");
-
-  // console.log("Book with standard 721 created at Library");
-
+  console.log("Secondary Collection contract was deployed to: ", secondaryCollection.address);
   
 }
 
